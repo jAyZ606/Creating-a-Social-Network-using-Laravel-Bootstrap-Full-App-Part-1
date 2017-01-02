160a9eb89ee6ce3f2e8dc06847b53d2ea5e2cb7b
jay galoyo <jay galoyo>
2017-01-02 22:10:35 +0800

    laravelnodashboard

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Illuminate\Contacts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model implements Authenticatable
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,47 @@
-<h1> The Dashboard </h1>
+@extends('layouts.master')
+
+@section('content')
+   <section class="row new-post">
+       <div class="col-md-6 col-md-offset-3">
+           <header><h3>What do you have to say?</h3></header>
+           <form action="">
+              <div class="form-group">
+                  <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+              </div>
+              <button type="submit" class="btn-primary"> Create Post</button>
+          </form>
+      </div>
+   </section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>what other people say....  </h3></header>
+            <article class="post">
+               <p>Lorem ipsum dolor sit amet.hula lahu nanugid ka sa iya sit.
+               </p>
+            <div class="info">
+            	Posted by Jay on 1 january 2017
+            </div>
+            <div class="interaction">
+            	<a href="#"> Like </a> |
+            	<a href="#"> Dislike </a> |
+            	<a href="#"> Edit </a> |
+            	<a href="#"> Delete </a>
+            </div>
+
+            </article>
+             <article class="post">
+               <p>Lorem ipsum dolor sit amet.hula lahu nanugid ka sa iya sit.
+               </p>
+            <div class="info">
+            	Posted by Jay on 1 january 2017
+            </div>
+            <div class="interaction">
+            	<a href="#"> Like </a> |
+            	<a href="#"> Dislike </a> |
+            	<a href="#"> Edit </a> |
+            	<a href="#"> Delete</a>
+            </div>
+
+            </article>
+        </div>
+   </section>
+@endsection
