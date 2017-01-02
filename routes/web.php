b39efa6c1ec6623faf489b8fac79cf929bde01f3
jay galoyo <jay galoyo>
2017-01-02 23:06:33 +0800

    creatingpost(model&controller)

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,8 @@ Route::group(['middleware' => ['web']], function () {
           'as' => 'dashboard',
           'middleware'=>'auth'
     	]);
+    Route::post('/createpost',[
+          'uses' => 'PostController@postCreatePost',
+          'as' => 'post.create'
+    	]);
 });
